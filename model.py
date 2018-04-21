5da401d41f66386ea9b5c6c956acf1e0e4b0112a
Isaac Tay <Isaac@makerforce.io>
2018-04-21 11:54:00 +0800

    Updated model

--- model.py
+++ model.py
@@ -5,15 +5,15 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, 5)
-        self.conv2 = nn.Conv2d(10, 20, 5)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 26)
+        self.conv1 = nn.Conv2d(1, 26, 5)
+        self.conv2 = nn.Conv2d(26, 52, 5)
+        self.fc1 = nn.Linear(832, 260)
+        self.fc2 = nn.Linear(260, 26)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = x.view(-1, 320)
+        x = x.view(-1, 832)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
